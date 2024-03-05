d4f84e2623a112f70f085bbd2888420b1196f5a0
Dani <dani96sk@hotmail.com>
2024-03-05 11:08:52 +0100

    Version 001
    Creamos las variables que almacenarán el RNG que se utilizará en cada iteración. Creamos además también los avisos de la vida de cada personaje y la interacción del usuario para pasar a la siguiente ronda

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,35 @@ public class Main {
 
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        while (vidaHeroe > 0 && vidaVampiro > 0) {
 
+            // Turno del heroe
+            int probH = random.nextInt(100);
+
+
+            if (vidaVampiro <= 0) {
+                System.out.println("El heroe ha derrotado al vampiro. ¡Victoria del heroe!");
+                break;
+            }
+
+            // Turno del vampiro
+            int probV = random.nextInt(100);
+
+
+            if (vidaHeroe <= 0) {
+                System.out.println("El vampiro ha derrotado al heroe. ¡Victoria del vampiro!");
+                break;
+            }
+
+            System.out.println("Estado actual de la batalla:");
+            System.out.println("Heroe HP: " + vidaHeroe);
+            System.out.println("Vampiro HP: " + vidaVampiro);
+            System.out.println();
+
+            System.out.print("Presiona Enter para continuar...");
+            scanner.nextLine();
+        }
+
+        scanner.close();
     }
 }
